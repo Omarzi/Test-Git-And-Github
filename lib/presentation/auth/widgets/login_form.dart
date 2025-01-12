73883a3091111eee365cad1356232713456f3dcf
Omar <3omarabdelaziz123@gmail.com>
2025-01-12 15:48:00 +0200

    Create Login Screen Code & Login Form

---
+++ lib/presentation/auth/widgets/login_form.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class LoginForm extends StatefulWidget {
+  const LoginForm({super.key});
+
+  @override
+  State<LoginForm> createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const TextField(),
+        const TextField(),
+        const TextField(),
+        ElevatedButton(onPressed: () {}, child: const Text('Login'))
+      ],
+    );
+  }
+}
